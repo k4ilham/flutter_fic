96553ea517a43bb864692adf6d8a70b4181e71b6
Ilham Maulana <k4ilham@gmail.com>
2022-12-04 21:53:05 +0700

    if statement

--- demo.dart
+++ demo.dart
@@ -1,6 +1,6 @@
-class Product {
-  static String productName = "";
-}
+// class Product {
+//   static String productName = "";
+// }
 
 void main() {
   // print("Hello world");
@@ -22,13 +22,43 @@ void main() {
   // double total = price * (qty ?? 0);
   // print(total);
 
-  var price = 25000;
-  var qty = 10;
-  var total = (price * qty) + 1000;
-  print(total);
+  // var price = 25000;
+  // var qty = 10;
+  // var total = (price * qty) + 1000;
+  // print(total);
 
-  var firstName = "Ilham";
-  var lastName = "Maulana";
-  var fullName = "$firstName $lastName";
-  print(fullName);
+  // var firstName = "Ilham";
+  // var lastName = "Maulana";
+  // var fullName = "$firstName $lastName";
+  // print(fullName);
+
+  // var total = 25000;
+  // if (total >= 25000) {
+  //   print("Anda termasuk boros");
+  // } else {
+  //   print("Anda lumayan hemat");
+  // }
+
+  // var total = 25000;
+  // var productName = "GG Filter 20";
+  // double discount = 0;
+  // if (total >= 25000 && productName == "GG Filter 20") {
+  //   discount = 0.1;
+  //   print("Anda termasuk potongan 10%");
+  // } else {
+  //   print("Anda tidak mendapatkan potongan");
+  // }
+
+  var total = 25000;
+  var productName = "ODOL CIPTADENT";
+  double discount = 0;
+  if (total >= 25000 && productName == "GG Filter 20") {
+    discount = 0.1;
+    print("Anda termasuk potongan 10%");
+  } else if (total >= 25000 && productName == "ODOL CIPTADENT") {
+    discount = 0.2;
+    print("Anda termasuk potongan 20%");
+  } else {
+    print("Anda tidak mendapatkan potongan");
+  }
 }
